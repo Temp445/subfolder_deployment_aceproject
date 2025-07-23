36f7d306f971cec5a2b26e3de48aa246e01a96c3
Temp445 <acetemp345@gmail.com>
2025-07-23 14:25:11 +0530

    first commit

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const nextConfig: NextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'px.ads.linkedin.com',  // for linkedin Tracking
+            },
+        ],
+        unoptimized: true, // ✅ disable image optimization on Netlify
+    },
+};
+
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(nextConfig);
